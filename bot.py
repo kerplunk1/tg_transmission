bce9f52c3987443febd3e29ea6d4ad071311a781
kerplunk1 <110846988+kerplunk1@users.noreply.github.com>
2024-12-27 13:03:22 +0400

    init commit
    init commit

---
+++ bot.py
@@ -0,0 +1,94 @@
+import requests
+from transmission_rpc import Client
+import re
+import time
+
+
+class Bot:
+    def __init__(self, bot_token, user_id):
+        self.user_id = user_id
+        self.token = bot_token
+        self.update_offset = None
+
+
+    def get_updates(self):
+        url = f"https://api.telegram.org/bot{self.token}/getUpdates"
+        if self.update_offset is None:
+            response = requests.get(url).json()['result']
+        else:
+            response = requests.get(url, params={'offset': self.update_offset}).json()['result']
+        if response:
+            for upd in response:
+                self.update_offset = int(upd['update_id']) + 1    
+            return response
+        else:
+            self.update_offset = None
+            return None
+        
+    
+    def send_answer(self, message_id, chat_id, text):
+        url = f"https://api.telegram.org/bot{self.token}/sendMessage"
+        params = {'reply_parameters': {'message_id': message_id}, 'chat_id': chat_id, 'text': text}
+        requests.post(url, json=params)
+    
+
+    def get_file(self, document):
+        if document is not None:
+            if document['mime_type'] == 'application/x-bittorrent':
+                path_url = f"https://api.telegram.org/bot{self.token}/getFile"
+                params = {"file_id": document["file_id"]}
+                file_path = requests.get(path_url, params=params).json()['result']['file_path']
+                
+                download_url = f"https://api.telegram.org/file/bot{self.token}/{file_path}"
+                f = requests.get(download_url).content
+                
+                new_downloading = torrent_client.add_torrent(f)
+                return f"{new_downloading.name}. The file has been added for download." 
+            else:
+                return 'Reject. Unknown file type.'
+    
+        
+    def handle_updates(self, updates):
+        if updates is not None:
+            for upd in updates:
+                message_id = upd['message']['message_id']
+                chat_id = upd['message']['chat']['id']
+                if self.user_id == int(upd['message']['from']['id']):
+
+                    document = self.get_file(upd['message'].get('document'))
+                    if document is not None:
+                        self.send_answer(message_id, chat_id, document)
+                        continue
+
+                    text = upd['message']['text']
+                    if text == 'list':
+                        active_torrents = torrent_client.get_torrents()
+                        info = []
+                        for t in active_torrents:
+                            info.append({'id': t.id, 'name': t.name, 'progress': t.progress})
+                        self.send_answer(message_id, chat_id, str(info))
+
+                    elif re.search(r'rm \d+', text):
+                        rm = text.split()
+                        torrent_client.remove_torrent(int(rm[1]))
+                        self.send_answer(message_id, chat_id, f'A request to delete file with ID {rm[1]} has been sent.')
+
+                    else:
+                        self.send_answer(message_id, chat_id, 'Rejected. Unknown request.')
+                else:
+                    self.send_answer(message_id, chat_id, 'Reject. Permission denied')
+
+
+    def start(self):
+        while True:
+            updates = self.get_updates()
+            self.handle_updates(updates)
+            time.sleep(300)
+
+
+
+torrent_client = Client(host="192.168.1.1", port=8090, username="", password="")
+
+bot = Bot("", )
+bot.start()
+
